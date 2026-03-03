678a7eb7a4fc8b9f34bdf6b39c53e9343e5ebf44
Amir Azizov <creator005@mail.ru>
2026-03-03 13:04:54 +0300

    add meta tags

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import { lusitana } from "./ui/fonts";
 import Image from "next/image";
 
 export default function Page() {
+  console.log('DB HOST:', process.env.POSTGRES_HOST);
+
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
